47563893dff15a29392eeae82a79d781d106e37b
Krishna Jain <jainkrishna79@gmail.com>
2022-07-22 13:34:47 +0530

    commit

---
+++ javascript.js
@@ -0,0 +1,30 @@
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 100}ms">${letter}</span>`
+    )
+    .join("");
+});
+
+// var img_slide = document.querySelector('body');
+var image = ["1", "2", "3", "4"];
+var i = image.length; //5
+
+function next() {
+  if (i < image.length) {
+    i = i + 1;
+  } else {
+    i = 1;
+  }
+  document.body.style.background = "url(" + image[i - 1] + ".jpg) no-repeat";
+  document.body.style.backgroundAttachment = "fixed";
+  document.body.style.backgroundSize = "cover";
+}
+
+setInterval(next, 5000);
+
+
